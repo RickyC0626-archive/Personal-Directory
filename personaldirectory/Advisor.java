4ac7aa622ecee6614c3b9eb98df91343ed726a91
nickclmaco <nickclimaco1618@gmail.com>
2018-10-25 17:07:48 -0400

    Nick made this

--- personaldirectory/Advisor.java
+++ personaldirectory/Advisor.java
@@ -6,7 +6,41 @@
    Authors: Ricky Chon, Nick Climaco, ZeJun Ren
 */
 
-public class Advisor extends Person
-{
+public class Advisor extends Person {
+  private String department;
+  private boolean isAcademicAdvisor;
+  private boolean isIndustryAdvisor;
+  private int yearsAsAdvisor;
 
+  public Advisor(String department, boolean isAcademicAdvisor, boolean isIndustryAdvisor, int yearsAsAdvisor) {
+    super();
+    this.department = department;
+    this.isAcademicAdvisor = isAcademicAdvisor;
+    this.isIndustryAdvisor = isIndustryAdvisor;
+    this.yearsAsAdvisor = yearsAsAdvisor;
+  }
+  public String getDepartment() {
+    return department;
+  }
+  public boolean getIsAcademicAdvisor() {
+    return isAcademicAdvisor;
+  }
+  public boolean getIsIndustryAdvisor() {
+    return isIndustryAdvisor;
+  }
+  public int getYearAsAdvisor() {
+    return yearsAsAdvisor;
+  }
+  public void setDepartment(String department) {
+    this.department = department;
+  }
+  public void setIsAcademicAdvisor(boolean isAcademicAdvisor) {
+    this.isAcademicAdvisor = isAcademicAdvisor;
+  }
+  public void setIsIndustryAdvisor(boolean isIndustryAdvisor) {
+    this.isIndustryAdvisor = isIndustryAdvisor;
+  }
+  public void setYearAsAdvisor(int yearsAsAdvisor) {
+    this.yearsAsAdvisor = yearsAsAdvisor;
+  }
 }
